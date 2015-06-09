a45b8fe54bb850f257ec0b87b92ded353e2718a2
Haipeng <njucs_wuhaipeng@163.com>
2015-06-09 18:24:19 +0800

    Add build scripts daocloud mysql/redis/mongo

--- build.sh
+++ build.sh
@@ -2,3 +2,13 @@
 
 docker build -t daocloud/base-image base-image
 docker build -t daocloud/ci-base ci-base
+
+# services
+docker build -t daocloud/ci-mysql:5.5 daocloud/ci-mysql/5.5
+docker build -t daocloud/ci-redis:2.8 daocloud/ci-redis/2.8
+docker build -t daocloud/ci-redis:2.6 daocloud/ci-redis/2.6
+docker build -t daocloud/ci-redis:3.0 daocloud/ci-redis/3.0
+docker build -t daocloud/ci-mongodb:2.2 daocloud/ci-mongodb/2.2
+docker build -t daocloud/ci-mongodb:2.4 daocloud/ci-mongodb/2.4
+docker build -t daocloud/ci-mongodb:2.6 daocloud/ci-mongodb/2.6
+docker build -t daocloud/ci-mongodb:3.0 daocloud/ci-mongodb/3.0
