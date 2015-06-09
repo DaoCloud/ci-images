261707800989fdcbd5f33edb117b86c7486090ca
Haipeng <njucs_wuhaipeng@163.com>
2015-06-09 18:34:11 +0800

    fix typo

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ docker build -t daocloud/base-image base-image
 docker build -t daocloud/ci-base ci-base
 
 # services
-docker build -t daocloud/ci-mysql:5.5 daocloud/ci-mysql/5.5
-docker build -t daocloud/ci-redis:2.8 daocloud/ci-redis/2.8
-docker build -t daocloud/ci-redis:2.6 daocloud/ci-redis/2.6
-docker build -t daocloud/ci-redis:3.0 daocloud/ci-redis/3.0
-docker build -t daocloud/ci-mongodb:2.2 daocloud/ci-mongodb/2.2
-docker build -t daocloud/ci-mongodb:2.4 daocloud/ci-mongodb/2.4
-docker build -t daocloud/ci-mongodb:2.6 daocloud/ci-mongodb/2.6
-docker build -t daocloud/ci-mongodb:3.0 daocloud/ci-mongodb/3.0
+docker build -t daocloud/ci-mysql:5.5 service/mysql/5.5
+docker build -t daocloud/ci-redis:2.6 service/redis/2.6
+docker build -t daocloud/ci-redis:2.8 service/redis/2.8
+docker build -t daocloud/ci-redis:3.0 service/redis/3.0
+docker build -t daocloud/ci-mongodb:2.2 service/mongodb/2.2
+docker build -t daocloud/ci-mongodb:2.4 service/mongodb/2.4
+docker build -t daocloud/ci-mongodb:2.6 service/mongodb/2.6
+docker build -t daocloud/ci-mongodb:3.0 service/mongodb/3.0
