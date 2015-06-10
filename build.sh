75f9c7581dd1548a3480b10f96a41c789f89adfb
Haipeng <njucs_wuhaipeng@163.com>
2015-06-10 17:02:39 +0800

    Add build script for python 2.7

--- build.sh
+++ build.sh
@@ -12,3 +12,6 @@ docker build -t daocloud/ci-mongodb:2.2 service/mongodb/2.2
 docker build -t daocloud/ci-mongodb:2.4 service/mongodb/2.4
 docker build -t daocloud/ci-mongodb:2.6 service/mongodb/2.6
 docker build -t daocloud/ci-mongodb:3.0 service/mongodb/3.0
+
+# platforms
+docker build -t daocloud/ci-python:2.7 platform/python/2.7
